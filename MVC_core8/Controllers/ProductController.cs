36da384f46a91c08bec2b59271dc8bee7129b3ea
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-09-27 18:32:18 +0530

    new lecture

---
+++ MVC_core8/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_core8.Models;
+
+namespace MVC_core8.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+                 var products = new List<Product>
+                 {
+                    new Product { ProductId = 1, Name = "Product A", Price = 100 },
+                    new Product { ProductId = 2, Name = "Product B", Price = 200 }
+                 };
+
+            return View(products);
+        }
+
+        public IActionResult Details( int? id )
+        {
+            var products = new List<Product>
+                 {
+                    new Product { ProductId = 1, Name = "Product A", Price = 100 },
+                    new Product { ProductId = 2, Name = "Product B", Price = 200 }
+                 };
+
+            return View(products);
+        }
+    }
+}
